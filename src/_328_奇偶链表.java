a74c4ea5e3fbbad2911675fce2974a851afe6d7f
shenchunxing <519045026@qq.com>
2022-12-21 22:11:53 +0800

    no message

---
+++ src/_328_奇偶链表.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.cn/problems/odd-even-linked-list/
+ */
+public class _328_奇偶链表 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return head;
+        //奇数链表
+        ListNode odd = head;
+        //偶数链表
+        ListNode even = head.next;
+        //保留偶数链表头
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;//奇偶两个链表串起来
+        return head;
+    }
+}
